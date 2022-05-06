13dc56677f6bf742c8f9dc2cbb4a939c9c8168ee
pascal <pmkgbwis@gmail.com>
2022-05-06 00:57:08 -0500

    Learnin some platforming

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,35 +4,59 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] SpriteRenderer sprite;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private SpriteRenderer sprite;
+    [SerializeField] private Rigidbody2D rbody;
+    [SerializeField] private Animator animator;
+    float jumpLockout;
+    float jumpLock = 0.2f;
+    [SerializeField] private float speed;
 
+    private bool grounded = false;
+    int jumps = 1;
     void FixedUpdate() {
+        CheckGrounded();
         Move();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        Jump();
     }
 
     private void Move() {
-        Vector3 pos = transform.position;
-        
+        rbody.velocity = rbody.velocity.y * Vector2.up;
+
+        bool running = false;
         if (Input.GetKey(KeyCode.D)) {
-            pos += new Vector3(0.1f, 0, 0);
+            rbody.velocity = new Vector2(speed, rbody.velocity.y);
             sprite.flipX = false;
+            running = true;
         }
         if (Input.GetKey(KeyCode.A)) {
-            pos += new Vector3(-0.1f, 0, 0); 
+            rbody.velocity = new Vector2(-speed, rbody.velocity.y); 
             sprite.flipX = true;
+            running = true;
         }
-       
-        transform.position = pos;
+
+        animator.SetBool("running", running);
+    }
+
+    private void Jump() {
+        if (Input.GetKeyDown(KeyCode.Space) && (grounded || jumps > 0)) {
+            rbody.velocity = new Vector2(rbody.velocity.x, 30);
+            jumps--;
+            jumpLockout = Time.time + jumpLock;
+            animator.SetBool("grounded", grounded && Time.time > jumpLockout);
+            animator.SetTrigger("jump");
+        }
+    }
+
+    private void CheckGrounded() {
+        bool wasGrounded = grounded;
+        grounded = Physics2D.Raycast((Vector2)transform.position - new Vector2(0, 1.5f), Vector2.down, 0.1f, LayerMask.GetMask("World"));
+        if (grounded)
+            jumps = 1;
+        if (grounded && !wasGrounded)
+            animator.SetTrigger("land");
+        animator.SetBool("grounded", grounded && Time.time > jumpLockout);
     }
 }
