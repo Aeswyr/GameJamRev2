352a340393bd2900344621488b158ddfe8d04870
cAdkins1997 <caadkins1997@gmail.com>
2022-05-10 19:50:05 -0500

    airdashboi

--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -6,17 +6,33 @@ using UnityEngine.InputSystem;
 public class InputHandler : MonoBehaviour
 {
     private Button m_primary;
-    public Button primary {
-        get {return m_primary;}
+    public Button primary
+    {
+        get { return m_primary; }
     }
     private Button m_jump;
-    public Button jump {
-        get {return m_jump;}
+    public Button jump
+    {
+        get { return m_jump; }
+    }
+
+    public Button m_dashmod;
+    public Button dashmod
+    {
+        get { return m_dashmod; }
+    }
+
+    public Button m_airdashmod;
+
+    public Button airdashmod
+    {
+        get { return m_airdashmod; }
     }
 
     private Vector2 m_dir;
-    public Vector2 dir {
-        get {return m_dir;}
+    public Vector2 dir
+    {
+        get { return m_dir; }
     }
 
     // Update is called once per frame
@@ -24,18 +40,29 @@ public class InputHandler : MonoBehaviour
     {
         m_jump.Reset();
         m_primary.Reset();
+        m_dashmod.Reset();
+        m_airdashmod.Reset();
     }
 
-    public void Jump(InputAction.CallbackContext ctx) {
+    public void Jump(InputAction.CallbackContext ctx)
+    {
         m_jump.Set(ctx);
     }
 
-    public void Primary(InputAction.CallbackContext ctx) {
+    public void Primary(InputAction.CallbackContext ctx)
+    {
         m_primary.Set(ctx);
     }
 
-    public void Move(InputAction.CallbackContext ctx) {
+    public void Move(InputAction.CallbackContext ctx)
+    {
         m_dir = ctx.ReadValue<Vector2>();
+    }
+
+    public void DashMod(InputAction.CallbackContext ctx)
+    {
+        m_dashmod.Set(ctx);
+        m_airdashmod.Set(ctx);
     }
 }
 
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -9,31 +7,123 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Animator animator;
     [SerializeField] private GameObject starPrefab;
     [SerializeField] private InputHandler input;
-    [SerializeField] private float speed;
+
+    [Header("Movement Variables")]
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float dashSpeed;
+    [SerializeField] private float airdashSpeed;
+    [SerializeField] private float startAirDashTime;
+    [SerializeField] private float gravity;
     
-    float jumpLockout;
-    float jumpLock = 0.2f;
+    [SerializeField] private int jumps = 2;
+    [SerializeField] private int airdashes = 1;
+
+    private float airDashTime;
+
+    private float speed;
+
+    private float jumpLockout;
+    private float jumpLock = 0.2f;
+
+    private int airdashingDir;
 
     private bool grounded = false;
-    int jumps = 2;
+
+
+    private void Start()
+    {
+        speed = walkSpeed;
+    }
+
     void FixedUpdate() {
         CheckGrounded();
-        Move();
-        Jump();
-        Action();
+        AirDashMod();
+        if (airDashTime <= 0)
+        {
+            Move();
+            Jump();
+            Action();
+            GroundDashMod();
+        }
     }
 
     private void Move() {
-        rbody.velocity = new Vector2(input.dir.x * speed, rbody.velocity.y);
+        Vector2 velocity = new Vector2(input.dir.x * speed, rbody.velocity.y);
+        if (grounded)
+        {
+            rbody.velocity = velocity;
+        }
+        else
+        {
+            if((Mathf.Sign(rbody.velocity.x) != Mathf.Sign(velocity.x)) 
+                || (velocity.x > rbody.velocity.x))
+            {
+                rbody.velocity = velocity;
+            }
+        }
+            bool running = input.dir.x != 0;
 
-        bool running = input.dir.x != 0;
-        
-        if (input.dir.x > 0)
-            sprite.flipX = false;
-        if (input.dir.x < 0)
-            sprite.flipX = true;
+            if (input.dir.x > 0)
+                sprite.flipX = false;
+            if (input.dir.x < 0)
+                sprite.flipX = true;
 
-        animator.SetBool("running", running);
+            animator.SetBool("running", running);
+    }
+
+    private void GroundDashMod()
+    {
+       if (input.dashmod.pressed)
+        {
+            speed = dashSpeed;
+        }
+       else if (input.dashmod.released)
+        {
+            speed = walkSpeed;
+        }
+
+    }
+
+    private void AirDashMod()
+    {
+        if (!grounded)
+        {
+            if (airdashingDir == 0)
+            {
+                if (input.airdashmod.pressed && input.dir.x != 0 && airdashes != 0)
+                {
+                    airdashes--;
+                    rbody.velocity = Vector2.right * rbody.velocity.x;
+                    airDashTime = startAirDashTime;
+                    airdashingDir = (int)input.dir.x;
+                    rbody.gravityScale = 0;
+                }
+            }
+            else
+            {
+                if (airDashTime <= 0)
+                {
+                   EndAirDash();
+                }
+                else
+                {
+                    airDashTime -= Time.deltaTime;
+                    rbody.velocity = Vector2.right * airdashSpeed * airdashingDir;
+                }
+            }
+        }
+        else if (airdashingDir != 0)
+        {
+            EndAirDash();
+        }
+    }
+
+    private void EndAirDash()
+    {
+        airdashingDir = 0;
+        airDashTime = 0;
+        rbody.velocity = Vector2.zero;
+        rbody.gravityScale = gravity;
     }
 
     private void Jump() {
@@ -58,7 +148,10 @@ public class PlayerController : MonoBehaviour
         bool wasGrounded = grounded;
         grounded = Physics2D.Raycast((Vector2)transform.position - new Vector2(0, 1.5f), Vector2.down, 0.1f, LayerMask.GetMask("World"));
         if (grounded)
+        {
             jumps = 2;
+            airdashes = 1;
+        }
         if (grounded && !wasGrounded)
             animator.SetTrigger("land");
         animator.SetBool("grounded", grounded && Time.time > jumpLockout);
